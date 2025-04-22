e269b9a0defd3a747bdeebcd92fcc852767ce5f6
mkeyhanim <MkeyhaniDeve@gmail.com>
2025-04-22 11:42:43 +0330

    write persist and devtools wrapper , for problem of hydration of Zustand write use method onRehydrationStorage

--- authentication/src/store/user.ts
+++ authentication/src/store/user.ts
@@ -1,24 +1,55 @@
 import { create } from "zustand";
+import { devtools, persist } from "zustand/middleware";
 
 interface AuthState {
   accessToken: string | null;
   refreshToken: string | null;
   expireTime: number | null;
+  loading: boolean;
   setAccessToken: (token: string) => void;
   setRefreshToken: (token: string) => void;
   setExpireTime: (time: number) => void;
+  setRefreshAndAccessAndExpireTime: (
+    refresh: string,
+    access: string,
+    expireTime: number
+  ) => void;
   clearTokens: () => void;
+  setLoading: (loading: boolean) => void;
 }
 
-const useAuthStore = create<AuthState>((set) => ({
-  accessToken: null,
-  refreshToken: null,
-  expireTime: null,
-  setAccessToken: (token) => set({ accessToken: token }),
-  setRefreshToken: (token) => set({ refreshToken: token }),
-  setExpireTime: (time) => set({ expireTime: time }),
-  clearTokens: () =>
-    set({ accessToken: null, refreshToken: null, expireTime: null }),
-}));
+const useAuthStore = create<AuthState>()(
+  devtools(
+    persist(
+      (set) => ({
+        accessToken: null,
+        refreshToken: null,
+        expireTime: null,
+        loading: true,
+        setAccessToken: (token) => set({ accessToken: token }),
+        setRefreshToken: (token) => set({ refreshToken: token }),
+        setExpireTime: (time) => set({ expireTime: time }),
+        setRefreshAndAccessAndExpireTime: (refresh, access, expireTime) =>
+          set({
+            accessToken: access,
+            refreshToken: refresh,
+            expireTime: expireTime,
+          }),
+        clearTokens: () =>
+          set({ accessToken: null, refreshToken: null, expireTime: null }),
+        setLoading: (loading) => set({ loading }),
+      }),
+      {
+        name: "auth-storage",
+        onRehydrateStorage: () => (state) => {
+          state?.setLoading(false);
+        },
+      }
+    ),
+    {
+      name: "AuthStore",
+    }
+  )
+);
 
 export default useAuthStore;
